294a2495f9ed43881d83770281784df5cc99a9ec
aphkyle <aphkyle@gmail.com>
2022-11-20 21:26:13 +0800

    fix

--- build.py
+++ build.py
@@ -1,31 +1,31 @@
-import os
 import pathlib
 import re
 import shutil
 import subprocess
 
-os.mkdir("dist")
+dist = pathlib.Path("dist")
+src  = pathlib.Path("src")
+dist.mkdir(exist_ok=True)
 
-src = pathlib.Path("src")
 for filepath in src.glob("*"):
     print(filepath)
     with open(filepath, encoding="utf8") as file:
         file_content = file.read()
     match filepath.suffix:
         case ".html":
-            with open(f"dist/{filepath.name}", "w", encoding="utf8") as f:
+            with open(dist/filepath.name, "w", encoding="utf8") as f:
                 f.write(re.sub("\n *", "", file_content))
         case ".map", ".scss":
             pass
         case ".js":
-            with open(f"dist/{filepath.name}", "w", encoding="utf8") as f:
+            with open(dist/filepath.name, "w", encoding="utf8") as f:
                 f.write(
                     subprocess.run(
                         ["uglifyjs", filepath], shell=True, capture_output=True
                     ).stdout.decode()
                 )
         case "css":
-            with open(f"dist/{filepath.name}", "w", encoding="utf8") as f:
+            with open(dist/filepath.name, "w", encoding="utf8") as f:
                 f.write(
                     subprocess.run(
                         ["cleancss", filepath], shell=True, capture_output=True
