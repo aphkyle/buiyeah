c86b53cb48db753e6e5e01627ac0a4373c2356f6
aphkyle <aphkyle@gmail.com>
2022-12-09 17:38:10 +0800

    fix build script and deploy

--- build.py
+++ build.py
@@ -9,31 +9,37 @@ src = pathlib.Path("src")
 dist.mkdir(exist_ok=True)
 
 for file in dist.glob("*"):
-  os.remove(file)
+    os.remove(file)
 
 for filepath in src.glob("*"):
+    if filepath.name == "nord.css": continue
     with open(filepath, encoding="utf8") as file:
         file_content = file.read()
     match filepath.suffix:
         case ".html":
-            with open(dist/filepath.name, "w", encoding="utf8") as f:
+            with open(dist / filepath.name, "w", encoding="utf8") as f:
                 f.write(re.sub("\n *", "", file_content))
         case ".scss":
             pass
         case ".js":
-            with open(dist/filepath.name, "w", encoding="utf8") as f:
+            with open(dist / filepath.name, "w", encoding="utf8") as f:
                 f.write(
                     subprocess.run(
-                        ["uglifyjs", filepath], shell=True, capture_output=True
-                    ).stdout.decode()
+                        ["uglifyjs", filepath],
+                        shell=True,
+                        capture_output=True,
+                        encoding="utf-8",
+                        text=True
+                    ).stdout
                 )
-        case "css":
-            with open(dist/filepath.name, "w", encoding="utf8") as f:
-                f.write(
-                    subprocess.run(
-                        ["cleancss", filepath], shell=True, capture_output=True
-                    ).stdout.decode()
-                )
+        case ".css":
+            subprocess.run(
+                ["cleancss", "-O2", filepath, '>', f"dist/{filepath.name}"],
+                shell=True,
+                capture_output=True,
+                encoding="utf-8",
+                text=True
+            ).stdout
         case _:
             shutil.copy2(filepath, f"dist/{filepath.name}")
     print(f"✅ {filepath}")
